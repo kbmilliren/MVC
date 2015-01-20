8a41555b8f010d5f1cd9624fd0e6ace624de158c
Bryant Milliren <kbmilliren@northstate.net>
2015-01-20 18:12:48 -0500

    First Commit

---
+++ mvc/mvc/Controllers/ExcercisesController.cs
@@ -0,0 +1,33 @@
+﻿using mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class ExcercisesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Sum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Sum(SumPostModel model)
+        {
+            var responseModel = new SumViewModel
+            {
+                number1 = model.number1,
+                number2 = model.number2
+
+            }; 
+                responseModel.SetSum();
+              
+                return View(responseModel);
+        }
+    } 
+    
+}
